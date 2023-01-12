8af885eadc04f5a38c49a6d92c65619346297191
kingdreamerr <jackmvula458@gmail.com>
2023-01-12 20:27:19 +0200

    Add a while loop

--- main.py
+++ main.py
@@ -14,28 +14,29 @@ for _ in word:
     display.append("_")
 
 print(logo)
-# Display the selected word with blanks 
-for dash in display:
-    print(dash, end=" ")
+while not end_of_game:
+    # Display the selected word with blanks 
+    for dash in display:
+        print(dash, end=" ")
 
-# get the user's guess
-guess = input("\n\nPlease guess a letter: ").lower()
+    # get the user's guess
+    guess = input("\n\nPlease guess a letter: ").lower()
 
-# if the guess is correct replace the _ in diplay with the guessed letter
+    # if the guess is correct replace the _ in diplay with the guessed letter
 
-for num in range(len(word)):
-        if word[num] == guess:
-            display[num] = guess
-# if the guess is incorrect remove a life
-if guess not in word:
-    lives -= 1
-    print(stages[lives])
+    for num in range(len(word)):
+            if word[num] == guess:
+                display[num] = guess
+    # if the guess is incorrect remove a life
+    if guess not in word:
+        lives -= 1
+        print(stages[lives])
 
-    if lives == 0:
+        if lives == 0:
+            end_of_game = True
+            print("You lose!")
+    print(f" You have {lives} lives remaining")
+    print(display)
+    if "_" not in display:
         end_of_game = True
-        print("You lose!")
-print(f" You have {lives} lives remaining")
-print(display)
-if "_" not in display:
-    end_of_game = True
-    print("you win!!")
+        print("you win!!")
