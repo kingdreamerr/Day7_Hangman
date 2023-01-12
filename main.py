df9ce5b567ceaa77edc1efb37cf74ebcfac09a00
kingdreamerr <jackmvula458@gmail.com>
2023-01-12 20:35:03 +0200

    Add logic to tell the user if he/she repeats a letter that they already guessed

--- main.py
+++ main.py
@@ -21,22 +21,25 @@ while not end_of_game:
 
     # get the user's guess
     guess = input("\n\nPlease guess a letter: ").lower()
-
+        # Check if the letter has alredy been guessed
+    if guess in display:
+        print(f"you have already guessed the letter {guess}")
     # if the guess is correct replace the _ in diplay with the guessed letter
 
     for num in range(len(word)):
             if word[num] == guess:
                 display[num] = guess
+
     # if the guess is incorrect remove a life
     if guess not in word:
         lives -= 1
+        print("You ussed wrongly you lose a life")
         print(stages[lives])
 
         if lives == 0:
             end_of_game = True
             print("You lose!")
     print(f" You have {lives} lives remaining")
-    print(display)
     if "_" not in display:
         end_of_game = True
         print("you win!!")
