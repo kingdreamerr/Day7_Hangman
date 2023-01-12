36888bd7e7ea5455e8b8f41d77db60bd52bb5644
kingdreamerr <jackmvula458@gmail.com>
2023-01-12 20:19:32 +0200

    Display the random word with dashes and get the user input

--- main.py
+++ main.py
@@ -3,6 +3,20 @@ from word_list import words
 from art import logo,stages
 
 # Get a random word
-
 word = random.choice(words)
-dispaly = []
+# create a variable to track the random word
+display = []
+end_of_game = False
+lives = 6
+
+# display the dashes representing the word
+for _ in word:
+    display.append("_")
+
+print(logo)
+# Display the selected word with blanks 
+for dash in display:
+    print(dash, end=" ")
+
+# get the user's guess
+guess = input("\n\nPlease guess a letter: ").lower()
